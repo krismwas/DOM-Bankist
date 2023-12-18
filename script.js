7c145b2c3cb9be94c38e146a8a72f2769a94d465
anonymous <krismwasgit@gmail.com>
2023-12-18 12:08:30 +0300

    event propagation

--- script.js
+++ script.js
@@ -36,3 +36,21 @@ const section1 = document.querySelector('#section--1');
 btnScrollTo.addEventListener('click', function () {
   section1.scrollIntoView({ behavior: 'smooth' });
 });
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains('nav__link')) {
+    console.log(e.target.getAttribute('href'));
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({ behaviour: 'smooth' });
+//   });
+// });
